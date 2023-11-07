17524ca996cd188a76d59a31c7d74ae2f8c307ee
Emma Gerigscott <elgerig@gmail.com>
2023-11-06 17:00:30 -0800

    add basic hero section component

---
+++ src/app/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const HeroSection = () => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2">
+      <div className="col-span-7 place-self-center">
+        <h1 className="mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I'm Emma</h1>
+        <p>Lorem ipsum my ass</p>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import { HeroSection } from './components/HeroSection'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Portfolio Website</h1>
+      <HeroSection />
     </main>
   )
 }
