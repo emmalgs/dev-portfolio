f29cbf3504391ee2d88d6c20b9fdec23e981f335
Emma Gerigscott <elgerig@gmail.com>
2023-11-07 10:01:36 -0800

    add prop types to menu overlay for ts

--- src/app/components/MenuOverlay.tsx
+++ src/app/components/MenuOverlay.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import NavLink from "./Navlink";
 import { NavLinkProps } from "./Navlink";
 
-const MenuOverlay = ({ links }) => {
+interface MenuOverlayProps {
+  links: NavLinkProps[];
+}
+
+const MenuOverlay: React.FC<MenuOverlayProps> = ({ links }) => {
   return (
-    <ul className="flex flex-col py-4 items-center">{links.map((link: NavLinkProps, index: number) => (
-      <li key={index}>
-        <NavLink href={link.href} title={link.title} />
-      </li>
-    ))}</ul>
-  )
+    <ul className="flex flex-col py-4 items-center">
+      {links.map((link: NavLinkProps, index: number) => (
+        <li key={index}>
+          <NavLink href={link.href} title={link.title} />
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default MenuOverlay;
