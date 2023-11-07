317d102f52f98be2c3adcf6cbe286279b6d8c815
Emma Gerigscott <elgerig@gmail.com>
2023-11-07 15:23:37 -0800

    Update HeroSection component styles and button borders

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -29,22 +29,22 @@ export const HeroSection = () => {
         </h1>
         <p className="text-base sm:text-lg mb-6 lg:text-xl">With a background in contemporary art, I bring out-of-the-box thinking to solving problems for both users and code that results in readable and innovative solutions.</p>
         <div>
-          <button className="font-mono px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-transparent hover:bg-blue-500 border border-white hover:border-stone-800">
+          <button className="font-mono px-6 py-3 w-full sm:w-fit mr-4 bg-transparent hover:bg-blue-500 border-2 border-black hover:border-stone-800">
             Connect
           </button>
-          <button className="font-mono px-6 py-3 w-full sm:w-fit rounded-full mt-2 bg-transparent hover:bg-stone-800 hover:text-white border border-white">
+          <button className="font-mono px-6 py-3 w-full sm:w-fit mt-2 bg-transparent hover:bg-stone-800 hover:text-white border-2 border-black">
             Github
           </button>
         </div>
       </div>
       <div className="col-span-5 place-self-center ml-4 mt-4 lg:mt-0">
-        <div className="rounded-full bg-stone-200 w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+        <div className="rounded-full bg-stone-300 w-[290px] h-[290px] lg:w-[400px] lg:h-[400px] relative">
           <Image
             src="/images/keyboard.gif"
             alt="hero-image"
             className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            width={180}
-            height={180}
+            width={200}
+            height={200}
           />
         </div>
       </div>
