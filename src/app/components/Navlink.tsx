c323dfd6db11111d3264946a8f14e00d1b085899
Emma Gerigscott <elgerig@gmail.com>
2023-11-07 09:55:49 -0800

    Update NavLink styling and export type definition

--- src/app/components/Navlink.tsx
+++ src/app/components/Navlink.tsx
@@ -9,11 +9,13 @@ const NavLink = ({ href, title }: NavLinkProps) => {
   return (
     <Link
       href={href}
-      className="block py-2 pl-3 pr-4 text-slate-500 sm:text-xl rounded md:p-0 hover:text-slate-800"
+      className="block py-2 pl-3 pr-4 text-slate-200 sm:text-xl rounded md:p-0 hover:text-slate-800"
     >
       {title}
     </Link>
   );
 };
 
-export default NavLink;
+export default NavLink 
+export type { NavLinkProps };
+
