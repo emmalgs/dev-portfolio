ede0c40f9e604ddabc67f86a0a3a313444d3e2d4
Emma Gerigscott <elgerig@gmail.com>
2023-11-07 13:26:07 -0800

    Add project tag filtering functionality to ProjectsSection component

--- src/app/components/ProjectsSection.tsx
+++ src/app/components/ProjectsSection.tsx
@@ -1,13 +1,42 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ProjectsCard from "./ProjectsCard";
 import { projects } from "../data/projectsData";
+import ProjectTag from "./ProjectTag";
 
 const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+
+  const handleTagChange = (newTag: string) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projects.filter((project) => project.tag.includes(tag))
+
   return (
     <div>
-      <h2 className="text-center text-4xl font-bold text-stone-700 mt-4 mb-4">My Projects</h2>
+      <h2 className="text-center text-4xl font-bold text-stone-700 mt-4 mb-4">
+        Projects
+      </h2>
+      <div className="flex flex-row jusitfy-center items-center gap-2 py-6">
+        <ProjectTag
+          onSelection={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onSelection={handleTagChange}
+          name="JS/TS"
+          isSelected={tag === "JS/TS"}
+        />
+        <ProjectTag
+          onSelection={handleTagChange}
+          name="C#"
+          isSelected={tag === "C#"}
+        />
+      </div>
       <div className="grid md:grid-cols-3 gap-8 md:gap-12">
-        {projects.map((project) => (
+        {filteredProjects.map((project) => (
           <ProjectsCard
             key={project.id}
             title={project.title}
