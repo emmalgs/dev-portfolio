0d5b2d4a3259b5e365c6aad9943df22b092a4426
Emma Gerigscott <elgerig@gmail.com>
2023-11-07 13:26:02 -0800

    Refactor ProjectsCard component styling

--- src/app/components/ProjectsCard.tsx
+++ src/app/components/ProjectsCard.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
@@ -26,7 +25,7 @@ const ProjectsCard: React.FC<ProjectsCardProps> = ({
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full hidden bg-stone-800 bg-opacity-0 group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
           <Link
             href={`${gitUrl}`}
-            className="h-14 w-14 border-2 relative rounded-full border-stone-300 hover:border-white group/link"
+            className="h-14 w-14 border-2 mr-2 relative rounded-full border-stone-300 hover:border-white group/link"
           >
             <CodeBracketIcon className="h-10 w-10 mr-2 text-stone-300 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
           </Link>
@@ -40,9 +39,9 @@ const ProjectsCard: React.FC<ProjectsCardProps> = ({
           )}
         </div>
       </div>
-      <div className="rounded-b-xl bg-stone-400 py-6 px-4">
-        <h5 className="text-xl font-semibold mb-2 text-stone-100">{title}</h5>
-        <p className="text-stone-700">{description}</p>
+      <div className="rounded-b-xl bg-stone-200 py-6 px-4 group-hover:bg-stone-300">
+        <h5 className="text-xl font-semibold mb-2 text-stone-900">{title}</h5>
+        <p className="text-stone-600">{description}</p>
       </div>
     </div>
   );
