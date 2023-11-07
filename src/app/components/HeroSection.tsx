c701519e9efad59417922afa2928a314a4882c41
Emma Gerigscott <elgerig@gmail.com>
2023-11-06 20:45:21 -0800

    edit margin + font fam in hero

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,26 +1,27 @@
 import React from "react";
 import Image from "next/image";
 
-const headerText = `Hello, I'm Emma`;
-
 export const HeroSection = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-12">
-      <div className="col-span-7 place-self-center">
-        <h1 className="mb-4 text-4xl lg:text-6xl font-extrabold">
-          {headerText}
+    <div className="grid grid-cols-1 sm:grid-cols-12">
+      <div className="col-span-7 place-self-center text-center sm:text-left">
+        <h1 className="mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold font-mono">
+          <span className="text-green-600">
+            Hello there, I'm{" "}
+          </span>
+          Emma
         </h1>
-        <p className="text-lg mb-6 lg:text-xl">Lorem ipsum my ass</p>
+        <p className="text-base sm:text-lg mb-6 lg:text-xl">Lorem ipsum git er done cmon give me some text and more thoughts and did you know that pythons have live births.</p>
         <div>
-          <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 border hover:border-slate-800">
-            Hire me
+          <button className="font-mono px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-transparent hover:bg-blue-500 border border-white hover:border-slate-800">
+            Connect
           </button>
-          <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 hover:text-white border border-white">
-            Download CV
+          <button className="font-mono px-6 py-3 w-full sm:w-fit rounded-full mt-2 bg-transparent hover:bg-slate-800 hover:text-white border border-white">
+            Github
           </button>
         </div>
       </div>
-      <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+      <div className="col-span-5 place-self-center ml-4 mt-4 lg:mt-0">
         <div className="rounded-full bg-slate-200 w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
           <Image
             src="/images/keyboard.gif"
