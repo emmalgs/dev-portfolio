12bfdadb6a9c6d85fadcb4519ae028eec77955b1
Emma Gerigscott <elgerig@gmail.com>
2023-11-07 12:14:11 -0800

    Add gitUrl and previewUrl to project data

--- src/app/data/projectsData.ts
+++ src/app/data/projectsData.ts
@@ -4,90 +4,116 @@ export const projects = [
     title: "Berli Designs",
     description: "A search engine for designers using the Chicago Art Institute's collection.",
     image: "/images/projects/1.png",
-    tag: ["all", "JS/TS"]
+    tag: ["all", "JS/TS"],
+    gitUrl: "",
+    previewUrl: "",
   },
   {
     id: 2,
     title: "Pierre's Bakery",
     description: "A console application for ordering from a bakery with fun ASCII animations",
     image: "/images/projects/2.png",
-    tag: ["all", "C#"]
+    tag: ["all", "C#"],
+    gitUrl: "",
+    previewUrl: "",
   },
   {
     id: 3,
     title: "Buckey's One Stop Art Shop",
     description: "A personalized art auction website",
     image: "/images/projects/3.png",
-    tag: ["all", "JS/TS"]
+    tag: ["all", "JS/TS"],
+    gitUrl: "",
+    previewUrl: "",
   },
   {
     id: 4,
     title: "Claire's Salon Manager",
     description: "A C# MVC website for managing a salon's stylists, clients, and appointments",
     image: "/images/projects/4.png",
-    tag: ["all", "C#"]
+    tag: ["all", "C#"],
+    gitUrl: "",
+    previewUrl: "",
   },
   {
     id: 5,
     title: "Cornshack",
     description: "Buy and sell corn, get a corn fact, add some corn!",
     image: "/images/projects/5.png",
-    tag: ["all", "JS/TS"]
+    tag: ["all", "JS/TS"],
+    gitUrl: "",
+    previewUrl: "",
   },
   {
     id: 6,
     title: "Currency Exchange",
     description: "Find current currency exchange rates",
     image: "/images/projects/6.png",
-    tag: ["all", "JS/TS"]
+    tag: ["all", "JS/TS"],
+    gitUrl: "",
+    previewUrl: "",
   },
   {
     id: 7,
     title: "Dragon Treasury",
     description: "A goofy collection of 'dragons' and their 'treasure'",
     image: "/images/projects/7.png",
-    tag: ["all", "JS/TS"]
+    tag: ["all", "JS/TS"],
+    gitUrl: "",
+    previewUrl: "",
   },
   {
     id: 8,
     title: "Factory Manager",
     description: "Manage a factory's machines and engineers",
     image: "/images/projects/8.png",
-    tag: ["all","C#"]
+    tag: ["all","C#"],
+    gitUrl: "",
+    previewUrl: "",
   },
   {
     id: 9,
     title: "Pet Shoppe",
     description: "A tamagochi-like pet collection game",
     image: "/images/projects/9.png",
-    tag: ["all", "C#"]
+    tag: ["all", "C#"],
+    gitUrl: "",
+    previewUrl: "",
   },
   {
     id: 10,
     title: "Pierre's Bakery Manager",
     description: "Bakery inventory and management website",
     image: "/images/projects/10.png",
-    tag: ["all", "C#"]
+    tag: ["all", "C#"],
+    gitUrl: "",
+    previewUrl: "",
   },
   {
     id: 11,
     title: "Buckey's Pizza Shop",
     description: "Build and order pizza with assistance of a lovable guinea pig",
     image: "/images/projects/11.png",
-    tag: ["all", "JS/TS"]
+    tag: ["all", "JS/TS"],
+    gitUrl: "",
+    previewUrl: "",
   },
   {
     id: 12,
     title: "Planet Ager",
     description: "Discover your age on other planets",
     image: "/images/projects/12.png",
-    tag: ["all", "JS/TS"]
+    tag: ["all", "JS/TS"],
+    gitUrl: "",
+    previewUrl: "",
   },
   {
     id: 13,
     title: "Prioritime",
     description: "Create a profile and organize your time",
     image: "/images/projects/13",
-    tag: ["all", "JS/TS"]
+    tag: ["all", "JS/TS"],
+    gitUrl: "",
+    previewUrl: "",
   }
 ];
