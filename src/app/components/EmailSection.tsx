e96c996d0bcce209b42ee97507217b9a662eff82
Emma Gerigscott <elgerig@gmail.com>
2023-11-07 16:27:42 -0800

    Fix formatting and add email input to EmailSection component.

--- src/app/components/EmailSection.tsx
+++ src/app/components/EmailSection.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import GitHubIcon from "../../../public/images/githubMove.png";
 import LinkedInIcon from "../../../public/images/linkedinMove.png";
 import Link from "next/link";
-import Image from "next/image"
+import Image from "next/image";
 
 const EmailSection = () => {
   return (
@@ -13,7 +13,14 @@ const EmailSection = () => {
         </h2>
         <p className="text-2xl text-black mb-4 md:mb-1 max-w-md font-mono">
           {" "}
-          <em>Talk to me!</em>
+          <em>
+            I'm actively seeking employment in the fields of web development and
+            software engineering. I'd love to hear from you! If you're looking
+            for my paintings and fine art, head over to{" "}
+            <Link className="text-blue-500" href="https://emmagerigscott.com">
+              my painting website
+            </Link>
+          </em>
         </p>
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/emmalgs">
@@ -23,18 +30,23 @@ const EmailSection = () => {
             <Image src={LinkedInIcon} alt="linkedin" width={50} height={50} />
           </Link>
         </div>
-        <form className="flex flex-col gap-4 mt-8">
-          <label className="text-black font-mono" htmlFor="email">Email</label>
-          <input
-            className="border-2 border-black rounded-sm px-4 py-2"
-            id="email"
-            type="email"
-            name="email" 
-            required
-            placeholder="dragon@fire.net"
-            />
-        </form>
       </div>
+      <form className="flex flex-col gap-4 mt-8">
+        <label
+          className="text-black block mb-2 text-sm font-mono"
+          htmlFor="email"
+        >
+          Email
+        </label>
+        <input
+          className="border-2 border-black rounded-sm px-4 py-2 block w-full"
+          id="email"
+          type="email"
+          name="email"
+          required
+          placeholder="dragon@fire.net"
+        />
+      </form>
     </section>
   );
 };
