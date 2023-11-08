ad22a314c41518b5151a1018d8350110215fe5ab
Emma Gerigscott <elgerig@gmail.com>
2023-11-08 11:39:31 -0800

    Add Footer component to project

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='footer bg-blue-600 border-r-transparent text-white'>
+      <div className='container p-12 flex justify-between'>
+      <p className='text-stone-300 font-mono text-sm'>© 2023 Emma Gerigscott - Ladies IT Department</p>
+      </div>
+    </footer>
+  );
+};
+
+
+export default Footer;
