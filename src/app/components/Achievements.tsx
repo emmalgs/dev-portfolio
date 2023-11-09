a96673d3de223830a8d24e39b4b6697dfe64f97a
Emma Gerigscott <elgerig@gmail.com>
2023-11-08 17:05:14 -0800

    Fix postfix for achievement values

--- src/app/components/Achievements.tsx
+++ src/app/components/Achievements.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 
@@ -8,20 +9,22 @@ const achievementData = [
   },
   {
     metric: "Projects",
-    value: "130+",
+    value: "130",
+    postfix: "+",
   },
   {
     metric: "Degrees",
-    value: "Too Many"
+    value: "Too Many",
   },
   {
     metric: "Cups of Coffee",
-    value: "1000+",
+    value: "1000",
+    postfix: "+",
   },
   {
     metric: "Hairless Guinea Pigs",
     value: "2",
-  }
+  },
 ];
 
 const techData = [
@@ -41,41 +44,45 @@ const techData = [
 const Achievements = () => {
   return (
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-      <div className="border-black border-2 rounded-sm py-8 px-17 flex flex-row flex-wrap items-center justify-evenly">
+      <div className="border-black border-2 rounded-sm py-8 px-16 flex flex-row flex-wrap items-center justify-evenly">
         {achievementData.map((achievement, index) => {
           return (
             <div
               key={index}
               className="flex flex-col items-center jusitfy-center mx-4"
             >
-              <h2 className="text-stone-700 text-4xl font-bold">
-                {achievement.value}
+              <h2 className="text-stone-700 text-4xl font-bold text-center">
+                {`${achievement.value} ${achievement.postfix ? achievement.postfix : ""}`}
               </h2>
               <p className="text-stone-900 text-base">{achievement.metric}</p>
             </div>
           );
         })}
-        </div>
-        <div className="border-black border-2 rounded-sm mt-10 py-8 px-17 flex flex-row flex-wrap items-center justify-evenly">
+      </div>
+      <div className="border-black border-2 rounded-sm mt-10 py-8 px-17 flex flex-row flex-wrap items-center justify-evenly">
         {techData.map((tech, index) => {
           return (
             <div
               key={index}
               className="flex flex-col items-center jusitfy-center mx-4"
             >
-              <Image 
-                src={`/images/icons/${tech === 'C#' ? 'Csharp' : tech === "Node.js" ? 'Node' : tech}.png`}
+              <Image
+                src={`/images/icons/${
+                  tech === "C#" ? "Csharp" : tech === "Node.js" ? "Node" : tech
+                }.png`}
                 alt={`${tech} logo`}
                 height={100}
                 width={100}
                 className="p-2"
               />
-              <h2 className="text-stone-700 text-base font-bold mb-4">{tech}</h2>
+              <h2 className="text-stone-700 text-base font-bold mb-4">
+                {tech}
+              </h2>
             </div>
           );
         })}
-  </div>
       </div>
+    </div>
   );
 };
 
