a6b39a89c519e1c61d777161d89d33137b42b233
Emma Gerigscott <elgerig@gmail.com>
2023-11-09 15:10:01 -0800

    Add animation to TabButton component

--- src/app/components/TabButton.tsx
+++ src/app/components/TabButton.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 interface TabButtonProps {
   active: boolean;
@@ -12,13 +13,21 @@ const TabButton: React.FC<TabButtonProps> = ({
   children,
 }) => {
   const buttonClasses = active
-    ? "text-stone-700 border-b border-green-500"
+    ? "text-stone-700"
     : "text-stone-500";
   return (
     <button onClick={selectTab}>
       <p className={`mr-3 font-semibold hover:text-blue-600 ${buttonClasses}`}>
         {children}
       </p>
+      <motion.div
+        animate={active ? "active" : "default"}
+        variants={{
+          default: { width: 0 },
+          active: { width: "calc(100% - 0.75rem)" },
+        }}
+        className="h-1 bg-green-500 mt-2 mr-3"
+      ></motion.div>
     </button>
   );
 };
