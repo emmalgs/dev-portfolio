de690096cc17e4bc739b807c1b7027350850a296
Emma Gerigscott <elgerig@gmail.com>
2023-11-09 15:41:19 -0800

    Add id to AboutSection

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -61,7 +61,7 @@ const AboutSection: React.FC = () => {
     });
   };
   return (
-    <section>
+    <section id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image
           src="/images/colorbg.png"
