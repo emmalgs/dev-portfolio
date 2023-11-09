ce0a84a00e94dbe955fa4b59a1a6cfb2a3480c32
Emma Gerigscott <elgerig@gmail.com>
2023-11-08 17:05:06 -0800

    Remove unnecessary border class from AboutSection component

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -61,7 +61,7 @@ const AboutSection: React.FC = () => {
     });
   };
   return (
-    <section className="border-t-2 border-black mt-6">
+    <section>
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image
           src="/images/colorbg.png"
